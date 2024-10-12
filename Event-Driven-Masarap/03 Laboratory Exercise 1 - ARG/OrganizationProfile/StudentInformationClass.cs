df0afe90e664f9021c568d79bb2efa75c46eaab7
ScubaScripter <145914099+YamiMitsuha@users.noreply.github.com>
2024-10-12 17:50:59 +0800

    Add files via upload

---
+++ Event-Driven-Masarap/03 Laboratory Exercise 1 - ARG/OrganizationProfile/StudentInformationClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Paala, James P.
+// BSIT - 501
+
+namespace OrganizationProfile
+{
+    internal class StudentInformationClass
+    {
+        public static int SetStudentNo = 0;
+        public static long SetContactNo = 0;
+        public static string SetProgram = "";
+        public static string SetGender = "";
+        public static string SetBirthDay = "";
+        public static string SetFullName = "";
+        public static int SetAge = 0;
+    }
+}
